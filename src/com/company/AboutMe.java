9bdd1b0206540f360de686dacd29a45835dc3b66
vova <volodymyrlakusta28@gmail.com>
2021-03-14 10:33:27 +0200

    Add AboutMe Class

--- src/com/company/AboutMe.java
+++ src/com/company/AboutMe.java
@@ -7,5 +7,30 @@ package com.company;/*
 */
 
 public class AboutMe {
-    
+    public static void main(String[] args) {
+
+        byte age = 21;
+        System.out.println("I am " + age + " years old.");
+
+        short yearOfBirth = 1999;
+        System.out.println("I was born in " + yearOfBirth);
+
+        int stepsPerDay = 8000;
+        System.out.println("I walk " + stepsPerDay + " every day");
+
+        long studentCardNumber = 22356515;
+        System.out.println("Student card number: " + studentCardNumber);
+
+        char surnameLetter = 'Y';
+        System.out.println("My surname first letter - " + surnameLetter);
+
+        float maxSpeed = 42.1f;
+        System.out.println("My max speed: " + maxSpeed);
+
+        double maxJumpHeight = 1.24;
+        System.out.println("My max jump height: " + maxJumpHeight);
+
+        boolean driverLicence = true;
+        System.out.println("Do you have driver licence: " + driverLicence);
+    }
 }
